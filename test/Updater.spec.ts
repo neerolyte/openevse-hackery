976ead71dda9eeb776d7248d5f0f5d2a88eea0a0
David Schoen <dave@lyte.id.au>
2021-11-29 12:34:26 +1100

    Add min constrain test

--- test/Updater.spec.ts
+++ test/Updater.spec.ts
@@ -1,33 +1,54 @@
-import { fail } from 'assert';
 import { expect } from 'chai';
-import * as sinon from 'ts-sinon'
+import * as sinon from 'ts-sinon';
+import { StubbedInstance } from 'ts-sinon';
 import { OpenevseClient } from '../src/OpenevseClient';
 import { SelectliveClient } from '../src/SelectliveClient';
 import { Updater } from '../src/Updater';
 
 describe('Updater', () => {
-  it('trusts OpenEVSE to constrain amps', async () => {
-    let selectliveClient = sinon.stubConstructor(SelectliveClient, { url: 'x-url', device: 'x-device'});
-    let openevseClient = sinon.stubConstructor(OpenevseClient, {
+  let selectliveClient: StubbedInstance<SelectliveClient>;
+  let openevseClient: StubbedInstance<OpenevseClient>;
+  let updater: Updater;
+  beforeEach(() => {
+    selectliveClient = sinon.stubConstructor(SelectliveClient, {
+      url: 'x-url',
+      device: 'x-device',
+    });
+    openevseClient = sinon.stubConstructor(OpenevseClient, {
       url: 'x-url',
       ampsMax: 20,
       ampsMin: 6,
     });
+    updater = new Updater(selectliveClient, openevseClient);
+  });
 
+  it('trusts OpenEVSE to constrain amps to max', async () => {
     selectliveClient.getBatterySoc.returns(Promise.resolve(100));
     selectliveClient.getBatteryW.returns(Promise.resolve(-240000));
     openevseClient.constrainAmps.returns(123);
-
-    let updater = new Updater(selectliveClient, openevseClient);
 
     let result = await updater.update();
 
     expect(result.spareAmps).to.equal(1005);
     expect(result.newTargetAmps).to.equal(123);
-
     let calls = openevseClient.setTargetAmps.getCalls();
     expect(calls.length).to.equal(1);
     expect(calls[0].args.length).to.equal(1);
     expect(calls[0].args[0]).to.equal(123);
+  });
+
+  it('trusts OpenEVSE to constrain amps to min', async () => {
+    selectliveClient.getBatterySoc.returns(Promise.resolve(100));
+    selectliveClient.getBatteryW.returns(Promise.resolve(0));
+    openevseClient.constrainAmps.returns(3);
+
+    let result = await updater.update();
+
+    expect(result.spareAmps).to.equal(5);
+    expect(result.newTargetAmps).to.equal(3);
+    let calls = openevseClient.setTargetAmps.getCalls();
+    expect(calls.length).to.equal(1);
+    expect(calls[0].args.length).to.equal(1);
+    expect(calls[0].args[0]).to.equal(3);
   });
 });
